01b3cfab03339c2233deaf28aae989b9381dd10c
Oleksandr Demchenko <email.root@gmail.com>
2016-08-17 10:34:24 +0300

    1

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kameron on 16.08.2016.
+ */
+var navigationPanels = document.getElementsByTagName('header');
+
+window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled){
+        navigationPanels[0].classList.add('nav__scrolled');
+    }else{
+        navigationPanels[0].classList.remove('nav__scrolled')
+    }
+}
