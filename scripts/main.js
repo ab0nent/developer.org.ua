0ef01c886d26ad5bdb6e233423fe260555b83dd9
Oleksandr Demchenko <email.root@gmail.com>
2016-09-02 11:19:27 +0300

    1

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,6 @@
 var navigationPanels = document.getElementsByTagName('header');
 
-window.onscroll = function() {
+window.onscroll = function () {
     var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrolled){
-        navigationPanels[0].classList.add('nav__scrolled');
-    }else{
-        navigationPanels[0].classList.remove('nav__scrolled')
-    }
+    scrolled ? navigationPanels[0].classList.add('nav__scrolled') : navigationPanels[0].classList.remove('nav__scrolled');
 }
